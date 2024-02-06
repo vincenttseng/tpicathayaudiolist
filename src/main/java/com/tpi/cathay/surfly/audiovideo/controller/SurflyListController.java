3e7c7c2e7db7eb151f24b029600ddf3da7ca211d
MSI\yuwen <vincent.tseng@tpisoftware.com>
2024-02-06 10:38:14 +0800

    ctrl

--- src/main/java/com/tpi/cathay/surfly/audiovideo/controller/SurflyListController.java
+++ src/main/java/com/tpi/cathay/surfly/audiovideo/controller/SurflyListController.java
@@ -1,11 +1,15 @@
 package com.tpi.cathay.surfly.audiovideo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.tpi.cathay.surfly.audiovideo.model.ImgReqModel;
 
 @Controller
@@ -17,8 +21,21 @@ public class SurflyListController {
 
 	}
 
+	@GetMapping(path = "/goViewSnap")
+	public String goViewSnap(Model model, @RequestParam(value = "SessionID", required = true) String sessionID,
+			@RequestParam(value = "TimeStamp", required = true) String timeStamp,
+			@RequestParam(value = "ConnID", required = true) String connID,
+			@RequestParam(value = "VoiceMountPath", required = true) String voiceMountPath) {
+		log.info("called {} {} {} {}", timeStamp, sessionID, connID, voiceMountPath);
+
+		ImgReqModel imgReqModel = ImgReqModel.builder().sessionID(sessionID).timeStamp(timeStamp).connID(connID)
+				.voicePath(voiceMountPath).build();
+
+		return viewSnap(imgReqModel);
+	}
+
 	@PostMapping("/snaplist")
-	public String viewBooks(@RequestBody ImgReqModel model) {
+	public String viewSnap(@RequestBody ImgReqModel model) {
 		log.info("called {}", model);
 
 		return "view-snaps";
--- src/main/java/com/tpi/cathay/surfly/audiovideo/model/ImgReqModel.java
+++ src/main/java/com/tpi/cathay/surfly/audiovideo/model/ImgReqModel.java
@@ -3,6 +3,7 @@ package com.tpi.cathay.surfly.audiovideo.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,19 +11,21 @@ import lombok.ToString;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+
 public class ImgReqModel {
 	@JsonProperty("SessionID")
 	private String sessionID;
-	
+
 	@JsonProperty("ConnID")
 	private String connID;
-	
+
 	@JsonProperty("TimeStamp")
 	private String timeStamp;
-	
+
 	@JsonProperty("VoiceMountPath")
 	private String voicePath;
 }
