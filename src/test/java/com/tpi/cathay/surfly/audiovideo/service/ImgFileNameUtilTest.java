02a29c4a9765dd55ca1705dec1265ffb03a075ff
MSI\yuwen <vincent.tseng@tpisoftware.com>
2024-02-06 14:17:56 +0800

    prod init setting

--- src/test/java/com/tpi/cathay/surfly/audiovideo/service/ImgFileNameUtilTest.java
+++ src/test/java/com/tpi/cathay/surfly/audiovideo/service/ImgFileNameUtilTest.java
@@ -9,10 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-
 import com.tpi.cathay.surfly.audiovideo.SpringBootJspApplication;
 
 @RunWith(SpringRunner.class)
